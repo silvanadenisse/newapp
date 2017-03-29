e5f9da6b0d5e2b544c774c23b4ed3eab8db58fe0
silvana.denisse.15@gmail.com <silvana.denisse.15@gmail.com>
2017-03-29 03:14:29 -0700

    editar crear

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-miblog_app:application.routes.draw do
+Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 end

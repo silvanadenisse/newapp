c9e3269b94355728ab0dcdffaed5aee047d1ac7c
silvana.denisse.15@gmail.com <silvana.denisse.15@gmail.com>
2017-03-29 03:55:56 -0700

    password

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329074229) do
+ActiveRecord::Schema.define(version: 20170329101949) do
 
   create_table "users", force: :cascade do |t|
     t.string   "nombre"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
